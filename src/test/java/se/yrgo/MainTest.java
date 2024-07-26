95e73652db8061738c52ceffab6f090f1e881974
elisan0107 <elisan0107@skola.goteborg.se>
2024-07-26 15:12:30 +0200

    Configuring pom.xml and hibernate.cfg.xml

---
+++ src/test/java/se/yrgo/MainTest.java
@@ -0,0 +1,11 @@
+package se.yrgo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
